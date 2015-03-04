d1aeab8885f77801151e0808b7b14396f8e85b74
Eric D Schmidt <schmidt.eric@gmail.com>
2015-03-04 10:03:19 -0800

    (feat) complete basic listing and creation of questions

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,4 +1,6 @@
 var Question = require('../models/question.js');
+var DocSet = require('../models/docSet.js');
+var DocElement = require('../models/docElement.js');
 
 module.exports = {
   load: function (req, res, next, questionID) {
@@ -7,7 +9,7 @@ module.exports = {
         req.question = question;
         next();
       } else {
-        res.status(404).send('Question ' + quesitonID  + ' not found');
+        res.status(404).send('Question ' + questionID  + ' not found');
       }
     });
   },
@@ -22,9 +24,30 @@ module.exports = {
   },
 
   create: function (req, res) {
-    Question.create(req.body).then(function(result) {
-      //TODO branch on result instanceof Sequelize.ValidaitonError
-      res.status(200).send(result);
+    DocSet.findOne({
+      where: {
+        name: req.body.docSetName
+      }
+    }).then(function(docSet) {
+      return DocElement.findOne({
+        where: {
+          name: req.body.docElementName,
+          DocSetId: docSet.id
+        }
+      });
+    }).then(function(docElement) {
+      var question = {
+        title: req.body.title,
+        text: req.body.text,
+        UserId: req.body.UserId,
+        DocElementId: docElement.id
+      }
+      Question.create(question).then(function(result) {
+        //TODO branch on result instanceof Sequelize.ValidaitonError
+        var createdQuestion = result.dataValues;
+        createdQuestion.Answers = [];
+        res.status(200).send(createdQuestion);
+      });
     });
   },
 
